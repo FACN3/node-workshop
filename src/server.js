7c64913d66749a256ce0aaac327a52fa80d0cedb
Ghassan <ghassan.maslamani@gmail.com>
2017-11-19 14:36:43 +0200

    adding handler function to the server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 var http = require('http');
 
-var server = http.createServer();
+var server = http.createServer(handler);
+
+
+function handler (request,response){
+
+  response.writeHead(200,{"Content-Type": "text/html"});
+  response.write("<h1> Ana sami mo 7ayala </h1>");
+  response.end();
+}
 
 server.listen(3010, function() {
   console.log("Server is listening on port 3010. Ready to accept requests!");
